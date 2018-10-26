8cf472cfa265e5cb2dddc6977b67f07944c068ff
ngweyeeshoon <ngweyeeshoon.1@gmail>
2018-10-26 13:51:25 +0630

    and login form with sharepreferences plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shared_preferences/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(new MyApp());
@@ -11,57 +12,9 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Login'),
+      home: new LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _loadCounter();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: RaisedButton(
-          onPressed: _increaseCounter,
-          child: new Text('Increment Counter'),
-        ),
-      ),
-    );
-  }
-
-  _increaseCounter() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _counter = (prefs.getInt('counter') ?? 0) + 1;
-      prefs.setInt('counter', _counter);
-    });
-  }
-
-  _loadCounter() async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    setState(() {
-      _counter = (preferences.getInt('counter') ?? 0);
-    });
-  }
-}
